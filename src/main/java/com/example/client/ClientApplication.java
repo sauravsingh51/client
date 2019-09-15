c704b6e28a814116e72a081790bc91b441a70d38
Saurav Singh <sauravsingh51@gmail.com>
2019-09-15 14:28:25 +0530

    Initial commit

---
+++ src/main/java/com/example/client/ClientApplication.java
@@ -0,0 +1,30 @@
+package com.example.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
+
+@RestController
+@RefreshScope
+class MessageRestController {
+	 
+    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+    private String msg;
+ 
+    @RequestMapping("/msg")
+    String getMsg() {
+        return this.msg;
+    }
+}
